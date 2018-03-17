8c37800e91e03ca189a9875b5aad86a95459907f
Rodrigo I. Ávila D <rodrig1a@yahoo.com>
2018-03-17 17:44:38 -0300

    Added common-ion effect
    Maybe it's time to split this with header files.

--- solubilidad_noionc.c
+++ solubilidad_noionc.c
@@ -11,12 +11,12 @@ long double conc = 1e-2;
 long double kPS = 0e+1;
 long double s_i_ans = 1e-1; //Can be any positive value I think
 
-//noionc recourses until the relative change of s_i
+//These functions recourse until the relative change of s_i
 //becomes lower than what PRECISION dictates.
 void noionc (long double a, long double b, long double c){
 	if ( 1 - (b / pow(10, -0.51*sqrt(c + a)))/c < PRECISION){
 		if (1 - (b / pow(10, -0.51*sqrt(c + a)))/c > -PRECISION) {
-			printf("La solubilidad es de %Le M\n", c);		
+			printf("La solubilidad es de %Le M\n", (b / pow(10, -0.51*sqrt(c + a))));
 			return;
 		}
 		else {
@@ -28,13 +28,38 @@ void noionc (long double a, long double b, long double c){
 	}
 }
 
+void ionc (long double a, long double b, long double c){
+	if (1 - (-a+sqrt(a*a+4*b/pow(10, -1.02*sqrt(c+a))))/(2*c) < PRECISION*100){
+		if (1 - (-a+sqrt(a*a+4*b/pow(10, -1.02*sqrt(c+a))))/(2*c) > -PRECISION*100){
+			printf("La solubilidad es de %Le M\n", ((-a+sqrt(a*a+4*b/pow(10, -1.02*sqrt(c+a))))/2));
+			return;
+		}
+		else {
+			ionc (a, b, ((-a+sqrt(a*a+4*b/pow(10, -1.02*sqrt(c+a))))/2));
+		}
+	}
+	else {
+		ionc (a, b, ((-a+sqrt(a*a+4*b/pow(10, -1.02*sqrt(c+a))))/2));
+	}
+}
+
 int main (){
+	char situation;
 	printf ("Se requiere de dos valores: La concentración actual\nde la solución y ");
 	printf ("la constante de solubilidad termodinámica del soluto.\n");
 	scanf ("%Le", &conc);
 	scanf ("%Le", &kPS);
-	kPS = sqrt(kPS);
-	//Placeholder for future switch to include common-ion effect and more.
-	noionc(conc, kPS, s_i_ans);
+	printf("¿Presenta efecto del ion común?: (s)i/(n)o\n");
+	getchar();
+	situation = getchar();
+	if ('n' == situation) {
+		kPS = sqrt(kPS);
+		noionc(conc, kPS, s_i_ans);
+	}
+	else if ('s' == situation) {
+		ionc(conc, kPS, s_i_ans);
+		
+	}
+	else {printf ("Caso no reconocido\n");}
 	return 0;
 }
